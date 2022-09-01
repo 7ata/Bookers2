65b1cf99f643b203ce105a50febbce917f69975f
7ata <103552711+7ata@users.noreply.github.com>
2022-09-01 12:10:13 +0000

    [Finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, presence: true
-  validates :introduction, presence: true
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
+  # nameの文字数は、1文字から10文字まで
+  validates :introduction, length: { maximum: 50 }
 
   def get_profile_image(width, height)
     unless profile_image.attached?
--- db/schema.rb
+++ db/schema.rb
@@ -41,9 +41,9 @@ ActiveRecord::Schema.define(version: 2022_08_06_094759) do
   end
 
   create_table "books", force: :cascade do |t|
-    t.integer "user_id"
     t.string "title"
     t.string "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
